9077039a635864197cb67f27473e91cc6159625e
Paolo Mason <mason.paolo@gmail.com>
2025-02-19 10:11:13 +0100

    added browser-sync

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-// Import our custom CSS
 import '../scss/styles.scss'
 
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
-'use strict';
-
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
-	// entry: './src/index.js',
 	entry: './src/js/main.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		// filename: 'index_bundle.js',
 		filename: 'main.js',
+		publicPath: '/',
 	},
 	module: {
 		rules: [
@@ -18,78 +16,62 @@ module.exports = {
 				test: /\.(png|jpe?g|gif|svg|webp)$/i,
 				type: 'asset/resource',
 				generator: {
-					filename: 'images/[name].[hash][ext]', // Output path for images
+					filename: 'images/[name].[hash][ext]',
 				},
 			},
 			{
-        test: /\.html$/i,
-        use: 'html-loader',
-      },
+				test: /\.html$/i,
+				use: 'html-loader',
+			},
 			{
-				// test: /\.s[ac]ss$/i,
 				test: /\.(scss)$/,
-				// use: [
-				// 	'style-loader',
-				// 	'css-loader',
-				// 	{
-				// 		loader: 'sass-loader',
-				// 		options: {
-				// 			api: 'modern-compiler',
-				// 			sassOptions: {
-				// 				// Your sass options
-				// 			},
-				// 		},
-				// 	},
-				// ],
 				use: [
+					'style-loader',
 					{
-						// Adds CSS to the DOM by injecting a `<style>` tag
-						loader: 'style-loader',
-					},
-					{
-						// Interprets `@import` and `url()` like `import/require()` and will resolve them
 						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							importLoaders: 2,
+						},
 					},
 					{
-						// Loader for webpack to process CSS with PostCSS
 						loader: 'postcss-loader',
 						options: {
 							postcssOptions: {
 								plugins: [autoprefixer],
 							},
+							sourceMap: true,
 						},
 					},
 					{
-						// Loads a SASS/SCSS file and compiles it to CSS
 						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+						},
 					},
 				],
 			},
-			// For development:
 			{
 				test: /\.css$/i,
-				use: [
-					'style-loader', // Injects CSS into the DOM
-					'css-loader', // Interprets CSS
-				],
+				use: ['style-loader', 'css-loader'],
 			},
 		],
 	},
-	plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
-	stats: {
-		warnings: false, // Disable warnings in the console
-	},
-	devServer: {
-		static: {
-			directory: path.resolve(__dirname, 'dist'),
-		},
-		port: 8080,
-		hot: true,
-		client: {
-			overlay: {
-				errors: false,
-				warnings: false,
+	plugins: [
+		new HtmlWebpackPlugin({ template: './src/index.html' }),
+		new BrowserSyncPlugin(
+			{
+				host: 'localhost',
+				port: 3000,
+				server: { baseDir: ['dist'] },
 			},
-		},
+			{
+				reload: true,
+			}
+		),
+	],
+	stats: {
+		warnings: false,
 	},
+	devtool: 'source-map',
 };
